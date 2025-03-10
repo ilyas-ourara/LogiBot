162410232dab31092963d47c877103ef2ceeaba2
ilyas ourara <ilyas.ourara@uit.ac.ma>
2025-03-10 15:24:20 +0000

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,13 +1,14 @@
 class Employee:
     
-    def __init__(self, name, salary):
+    def __init__(self, name, salary,hours):
         self.name = name
         self.salary = salary
+        self.hours=hours
         
     def display(self):
-        print("Name: ", self.name, ", Salary: ", self.salary)
+        print(f"Name: ", self.name, ", Salary: ", self.salary,"he works  {self.hours} per week")
 
 # Create an object of the class
-emp = Employee("John", 1000)
+emp = Employee("John", 1000,8)
 emp.display()
 
